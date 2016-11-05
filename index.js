7dda164753c9f4d32d6a726a314b3234015ad839
Jared Buckner <buckner.jared@gmail.com>
2016-11-05 18:26:59 -0500

    create public static directory. add handlebars.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // index.js
+var express = require('express')
+var app = express()
 
+app.get('/', function(req, res){
+	res.send('Hello, World');
+});
 
-require('./app/index')
+app.use(express.static('public'))
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port)
