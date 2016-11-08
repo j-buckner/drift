feadd43ed9a4cf9f584c313deb2653462bba295a
Jared Buckner <buckner.jared@gmail.com>
2016-11-07 22:16:23 -0600

    comment sass

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.get('/', function(req, res){
 
 app.use(express.static('public'));
 app.use('/public', express.static(__dirname + '/node_modules/materialize-css/dist'));
-app.use(
-     sass.middleware({
-         src: __dirname + '/sass', //where the sass files are 
-         dest: __dirname + '/public', //where css should go
-         debug: true // obvious
-     })
- );
+// app.use(
+//      sass.middleware({
+//          src: __dirname + '/sass', //where the sass files are 
+//          dest: __dirname + '/public', //where css should go
+//          debug: true // obvious
+//      })
+//  );
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port)
