da3faaa5d21427840044d50e74764f8bed2dd95b
Jared Buckner <buckner.jared@gmail.com>
2016-11-19 14:35:13 -0600

    cleanup carousel. add some styling

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-	$('.carousel.carousel-slider').carousel({full_width: true, time_constant: 700});
-	// $('.carousel').carousel({time_constant: 800});
-	setTimeout(autoplay, 4500);
+  $('.carousel.carousel-slider').carousel({full_width: true, time_constant: 600});
+  setTimeout(autoplay, 4500);
 });
 
 function autoplay() {
-    $('.carousel').carousel('next');
-    setTimeout(autoplay, 4500);
+  $('.carousel').carousel('next');
+  setTimeout(autoplay, 4500);
 }
