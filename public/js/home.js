36f645db64ae52fb3c3de88ca1dcd197cdded0d5
Jared Buckner <buckner.jared@gmail.com>
2016-11-19 17:48:25 -0600

    add bourbon. redo landing page using keystroke

--- public/js/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.carousel.carousel-slider').carousel({full_width: true, time_constant: 600});
-  setTimeout(autoplay, 4500);
-});
-
-function autoplay() {
-  $('.carousel').carousel('next');
-  setTimeout(autoplay, 4500);
-}
