a1fbeac094561e057e5835c3b8132e71102bd151
Jared Buckner <buckner.jared@gmail.com>
2016-11-24 20:51:43 -0600

    Add google maps api with basic map init

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const ARC_DE_TRIOMPHE_POSITION = {
+  lat: 48.873947,
+  lng: 2.295038
+};
+
+const EIFFEL_TOWER_POSITION = {
+  lat: 48.858608,
+  lng: 2.294471
+};
  
 class App extends React.Component {
   render() {
-    return <h1>Test App</h1>
+    return (
+      <div>
+        <Map />
+      </div>
+    )
+  }
+}
+
+class Map extends React.Component {
+  componentDidMount() {
+    this.map = new google.maps.Map(this.refs.map, {
+      center: EIFFEL_TOWER_POSITION,
+      zoom: 16
+    });
+  }
+  render() {
+    const mapStyle = {
+      width: '100%',
+      margin: '0 auto',
+      height: '100vh',
+      border: '1px solid black'
+    };
+    
+    return (
+      <div ref="map" style={mapStyle} ref="map">I should be a map!</div>
+    );
   }
 }
  
--- public/bundle.js
+++ public/bundle.js
@@ -21458,11 +21458,23 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+	function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var ARC_DE_TRIOMPHE_POSITION = {
+	  lat: 48.873947,
+	  lng: 2.295038
+	};
+
+	var EIFFEL_TOWER_POSITION = {
+	  lat: 48.858608,
+	  lng: 2.294471
+	};
 
 	var App = function (_React$Component) {
 	  _inherits(App, _React$Component);
@@ -21477,14 +21489,52 @@
 	    key: 'render',
 	    value: function render() {
 	      return _react2.default.createElement(
-	        'h1',
+	        'div',
 	        null,
-	        'Test App'
+	        _react2.default.createElement(Map, null)
 	      );
 	    }
 	  }]);
 
 	  return App;
+	}(_react2.default.Component);
+
+	var Map = function (_React$Component2) {
+	  _inherits(Map, _React$Component2);
+
+	  function Map() {
+	    _classCallCheck(this, Map);
+
+	    return _possibleConstructorReturn(this, (Map.__proto__ || Object.getPrototypeOf(Map)).apply(this, arguments));
+	  }
+
+	  _createClass(Map, [{
+	    key: 'componentDidMount',
+	    value: function componentDidMount() {
+	      this.map = new google.maps.Map(this.refs.map, {
+	        center: EIFFEL_TOWER_POSITION,
+	        zoom: 16
+	      });
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var mapStyle = {
+	        width: '100%',
+	        margin: '0 auto',
+	        height: '100vh',
+	        border: '1px solid black'
+	      };
+
+	      return _react2.default.createElement(
+	        'div',
+	        _defineProperty({ ref: 'map', style: mapStyle }, 'ref', 'map'),
+	        'I should be a map!'
+	      );
+	    }
+	  }]);
+
+	  return Map;
 	}(_react2.default.Component);
 
 	_reactDom2.default.render(_react2.default.createElement(App, null), document.getElementById('app'));
