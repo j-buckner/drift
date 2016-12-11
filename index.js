892e7f6ef4f4ff7dd6998c82a5ba7e22a7d8cb2d
Jared Buckner <buckner.jared@gmail.com>
2016-12-10 18:00:50 -0600

    install mongodb + mongoose and get connection working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require('express')
 var exphbs  = require('express-handlebars');
 var favicon = require('serve-favicon');
+var mongoose = require('mongoose');
 
 var app = express();
 app.use(favicon(__dirname + '/public/img/favicon.ico'));
@@ -19,6 +20,23 @@ app.get('/travel-profile', function(req, res){
 
 app.use(express.static('public'));
 app.use('/public', express.static(__dirname + '/node_modules/materialize-css/dist'));
+
+// Connection URL
+var url = 'mongodb://localhost:27017/drift';
+
+// Use connect method to connect to the server
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  var continentSchema = mongoose.Schema({
+    name: String
+  });
+
+  var Continent = mongoose.model('Continent', continentSchema);
+  var europe = new Continent({ name: 'Europe' });
+
+});
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port)
